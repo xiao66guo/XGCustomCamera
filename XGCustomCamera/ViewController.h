0dfedc8e7bd35bfbc249705024b8cd4dbf076c0a
一个老兵 <xiao66guo@163.com>
2016-12-06 19:56:49 +0800

    Initial Commit

---
+++ XGCustomCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XGCustomCamera
+//
+//  Created by 小果 on 2016/12/6.
+//  Copyright © 2016年 小果. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XGCustomCamera/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XGCustomCamera
+//
+//  Created by 小果 on 2016/12/6.
+//  Copyright © 2016年 小果. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
