6724cc99f5fa03934cbcaedb9abdb510eef06d31
一个老兵 <xiao66guo@163.com>
2016-12-08 21:09:48 +0800

    创建字体颜色选择控制器

---
+++ XGCustomCamera/XGSwitchColorController.h
@@ -0,0 +1,13 @@
+//
+//  XGSwitchColorController.h
+//  XGCustomCamera
+//
+//  Created by 小果 on 2016/12/8.
+//  Copyright © 2016年 小果. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XGSwitchColorController : UIViewController
+
+@end
---
+++ XGCustomCamera/XGSwitchColorController.m
@@ -0,0 +1,22 @@
+//
+//  XGSwitchColorController.m
+//  XGCustomCamera
+//
+//  Created by 小果 on 2016/12/8.
+//  Copyright © 2016年 小果. All rights reserved.
+//
+
+#import "XGSwitchColorController.h"
+
+@interface XGSwitchColorController ()
+
+@end
+
+@implementation XGSwitchColorController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
